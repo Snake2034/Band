68708d8662fcc311fcaf45630bcbf328bd2c78be
Getwrongg <35390957+Getwrongg@users.noreply.github.com>
2019-06-19 09:14:34 +0800

    change

--- Website/index.js
+++ Website/index.js
@@ -2,21 +2,33 @@ myApp = new Framework7();
 
 // Get the modal
 var modal = document.getElementById("myModal");
+var modal1 = document.getElementById("registerModal");
+
 
 // Get the button that opens the modal
 var btn = document.getElementById("loginButton");
+var btn1 = document.getElementById("registerButton");
+
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("Close")[0];
+var span1 = document.getElementsByClassName("registerClose")[0];
 
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
+btn1.onclick = function(){
+	modal1.style.display = "block";
+}
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+}
+span1.onclick = function() {
+  modal1.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -26,7 +38,3 @@ window.onclick = function(event) {
   }
 }
 
-function register()
-{
-	window.location.href = "register.html";
-}
--- Website/register.js
+++ Website/register.js
@@ -1,20 +1,3 @@
 myApp = new Framework7();
 
-var modal = document.getElementById("myModal");
-window.onload = function(){
 
-  modal.style.display = "block";
-
-}
-
-
-
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  window.location.href = "index.html";
-
-}
