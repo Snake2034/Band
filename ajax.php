19d78635bc7f3334886ee051b8767113afdc637e
Snake2034 <35478046+Snake2034@users.noreply.github.com>
2019-06-21 17:08:17 +0800

    Update Fixed Register

--- ajax.php
+++ ajax.php
@@ -1,17 +1,7 @@
 <?php
-	include ("action.php");
-
 	$name = $_REQUEST['Name'];
-		$email = $_REQUEST['Email'];
-		$pass = $_REQUEST['Password'];
-
-		echo "";
-		echo "Name Entered was: " . $name;
-		echo "<br>";
-		echo " And the email you entered was: " . $email;
-		echo "<br>";
-		echo "The Password you entered was: " . $pass;
-		echo "<br>";
+	$email = $_REQUEST['Email'];
+	$pass = $_REQUEST['Password'];
 
 	$servername = "localhost";
 	$username = "root";
@@ -27,39 +17,26 @@
 	{
 	    die("Connection failed: " . $conn->connect_error);
 	} 
-		echo "Connected successfully <br>";
-		echo mysqli_error($conn);
 	$dupCheck = "SELECT COUNT(*) FROM accounts WHERE Name = '$name' AND Email = '$email'";
 	$dupCheck2 = "SELECT COUNT(*) FROM accounts";
 
-	$re2 = mysqli_query($conn,$dupCheck2);
 	$r=mysqli_query($conn,$dupCheck);
-	
-	echo mysqli_error($conn);
-
-	/* Custom made error debugging references
-	$yeh = $re2->fetch_array();
 	$re = $r->fetch_array();
-	echo $re[0];
-	echo "<br>";
-	echo $yeh[0];
-	*/
-
+	
 	//Now to check, we use an if() statement
 	if($re[0] != 0)
 	{
-	 print "Record exists";
+	 	echo "Account already exists";
 	}
 	else if($re[0] == 0)
 	{
-	 print "Record doesn't exist";
+		echo "Account doesn't exist";
 
 	 //query for insert into db
 		$sql = "INSERT INTO accounts (Name, Email, Password) VALUES ('$name', '$email', '$pass')";
 
 	//Try and insert into db
 	if ($conn->query($sql) === TRUE) {
-    	echo "New record created successfully";
 	} 
 	else
 	{
