2430b639583e56628a7e7754794d9ba300a2636b
Getwrongg <35390957+Getwrongg@users.noreply.github.com>
2019-06-24 13:02:59 +0800

    jacob initial 1

--- band/src/app/app-routing.module.ts
+++ band/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GetStartedComponent } from './get-started/get-started.component';
+import { MembersComponent } from './members/members.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'getStarted', component: GetStartedComponent},
+  {path: 'members', component: MembersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- band/src/app/app.module.ts
+++ band/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { GetStartedComponent } from './get-started/get-started.component';
+import { HomeComponent } from './home/home.component';
+import { MembersComponent } from './members/members.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    GetStartedComponent,
+    HomeComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
